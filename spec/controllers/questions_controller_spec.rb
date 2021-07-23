dfe5767d01164383c06b29a45ac287afcd2c5c70
maxim <maximsandulsky@gmail.com>
2021-07-23 21:54:11 +0300

    QuestionController tests and simple actions added

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
@@ -10,6 +12,103 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'render index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'render show view' do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: question } }
+
+    it 'render edit view' do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question in database' do
+        count = Question.count
+
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+
+      it 'redirect to created question' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'didn`t save question' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+
+      it 'render new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+
+      it 'changed attributes' do
+        patch :update, params: { id: question, question: { title: 'new title', body: 'new body' } }
+        question.reload
+
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new body'
+      end
+
+      it 'redirect to updated question' do
+        patch :update, params: { id: question, question: attributes_for(:question) }
+        expect(request).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      before { patch :update, params: { id: question, question: attributes_for(:question, :invalid) } }
+
+      it 'didn`t change' do
+        question.reload
+
+        expect(question.title).to eq 'Some question'
+        expect(question.body).to eq 'Some desctription'
+      end
+
+      it 'render edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+
+    it 'delete question' do
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirect to index' do
+      delete :destroy, params: { id: question }
+
+      expect(response).to redirect_to questions_path
     end
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :question do
     title { "Some question" }
     body { "Some desctription" }
+
+    trait :invalid do
+      title { nil }
+    end
   end
 end
