2d86bfd730b3101ad762145312eaf4f490ff8c9d
maxim <maximsandulsky@gmail.com>
2021-10-24 15:33:09 +0300

    Jobs

---
+++ spec/jobs/reputation_job_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe ReputationJob, type: :job do
+  let(:question) { create(:question) }
+
+  it "calls ReputationService" do
+    expect(ReputationService).to receive(:calculate).with(question)
+    ReputationJob.perform_now(question)
+  end
+end
